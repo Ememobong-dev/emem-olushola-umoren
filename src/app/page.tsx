630047572eb424b69c8c237f8ed57fcec1933f42
Olushola <ememobongumoren2@gmail.com>
2025-04-10 00:06:12 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,23 @@ export default function Home() {
     <div>
       <Navbar />
       <div className="landing_bg relative">
-        <div className="absolute bottom-20">
-          <h2>Umoren</h2>
-          <h2>Ememobong</h2>
-          <p>
-          Self-taught Frontend Developer and Data Analyst skilled in React, Next.js, TypeScript, and TailwindCSS.
-          </p>
+        <div className="absolute bottom-20 px-14 3xl:px-28 w-full">
+          <div className="flex justify-between items-end w-full">
+            <div>
+              <h2 className="text-8xl">Umoren</h2>
+              <h2 className="text-8xl">Ememobong</h2>
+              <p className="w-[70%]">
+                Self-taught Frontend Developer and Data Analyst skilled in{" "}
+                <span className="text-pepper-red">React</span>,{" "}
+                <span className="text-blue">Next.js</span>,{" "}
+                <span className="text-cyan">TypeScript</span>, and{" "}
+                <span className="text-sharp-yellow">TailwindCSS</span>.
+              </p>
+            </div>
+            <div>
+              <button className="rounded-full border border-white py-2 px-6">Scroll down</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
