904b1ad7286f8456e593216f8f65902da6a05203
Olushola <ememobongumoren2@gmail.com>
2025-04-09 23:52:38 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="landing_bg h-screen">
-
+      <div className="landing_bg relative">
+        <div className="absolute bottom-20">
+          <h2>Umoren</h2>
+          <h2>Ememobong</h2>
+          <p>
+          Self-taught Frontend Developer and Data Analyst skilled in React, Next.js, TypeScript, and TailwindCSS.
+          </p>
+        </div>
       </div>
-
     </div>
   );
 }
