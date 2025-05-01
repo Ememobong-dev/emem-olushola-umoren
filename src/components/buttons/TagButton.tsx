c41f959a71d29fb1e400c00a63ea6b62530cbd60
Olushola <ememobongumoren2@gmail.com>
2025-05-01 09:31:00 +0100

    update

--- src/components/buttons/TagButton.tsx
+++ src/components/buttons/TagButton.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
-export const TagButton = ({ text }: { text: string }) => {
-  const [active, setActive] = useState(false);
+export const TagButton = ({ text, isActive, onClick }: { text: string; isActive: boolean; onClick: () => void }) => {
 
   const baseClasses =
     "rounded-full text-[12px] text-white border lg:border-0 opacity-25 font-azeret-mono cursor-pointer font-light  flex justify-center items-center bg-dark py-2 px-3 3xl:py-2 3xl:px-8 hover:text-white hover:opacity-100";
 
   return (
     <button
-      onClick={() => setActive(!active)}
+      onClick={onClick}
       className={`${baseClasses} ${
-        active ? "border border-white text-white opacity-100" : ""
+        isActive ? "border border-white text-white opacity-100" : ""
       }`}
     >
       <p>{text} </p>
