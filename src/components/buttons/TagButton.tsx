777521af6fb89d70b0d3d06def92e5b11ad20496
Olushola <ememobongumoren2@gmail.com>
2025-05-02 09:14:47 +0100

    update on vercel fail

--- src/components/buttons/TagButton.tsx
+++ src/components/buttons/TagButton.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-export const TagButton = ({ text, isActive, onClick }: { text: string; isActive: boolean; onClick: () => void }) => {
+export const TagButton = ({ text, isActive, onClick }: { text: string; isActive?: boolean; onClick?: () => void }) => {
 
   const baseClasses =
     "rounded-full text-[12px] text-white border lg:border-0 opacity-25 font-azeret-mono cursor-pointer font-light  flex justify-center items-center bg-dark py-2 px-3 3xl:py-2 3xl:px-8 hover:text-white hover:opacity-100";
