e0fa270258b63c5e10deda560a7e78988a6538cb
olushola <ememobongumoren2@gmail.com>
2025-06-23 04:10:05 +0100

    fixed build err

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -18,7 +18,7 @@ export default function NotFound() {
                         Page Not Found
                     </h2>
                     <p className={`${theme === "light" ? "text-gray-600" : "text-white"}  mb-8 max-w-md`}>
-                        Oops! The page you're looking for doesn't exist or has been moved.
+                        Oops! The page you&apos;re looking for doesn&apos;t exist or has been moved.
                     </p>
                     <Link
                         href="/"
