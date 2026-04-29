5280969368b2631c45fc3e4336c7bba12a70969c
Olushola <ememobongumoren2@gmail.com>
2026-04-29 13:20:01 +0100

    removed redundant files

--- src/components/MdxRenderer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useMemo } from 'react';
-import { getMDXComponent } from 'mdx-bundler/client/index.js'; // Don't omit index.js!
-
-export default function MdxRenderer({ code }: { code: string }) {
-  const Component = useMemo(() => getMDXComponent(code), [code]);
-  return <Component />;
-}
--- src/lib/useMDXComponent.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react';
-import { getMDXComponent } from 'mdx-bundler/client';
-
-export function useMDXComponent(code: string) {
-  return useMemo(() => getMDXComponent(code), [code]);
-}
